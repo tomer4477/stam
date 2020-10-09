1c8f5083f3e4060c8006ab14e603d0963710ac45
maorRoz <andorys@gmail.com>
2020-10-09 14:45:50 +0300

    added foo

--- Main.java
+++ Main.java
@@ -2,6 +2,10 @@ package src;
 
 
 Class Main {
+    public static void foo(){
+        System.out.println("Some changes!");
+    }
+
     public static void main(String[]args){
         System.out.println("Hello World");
     }
